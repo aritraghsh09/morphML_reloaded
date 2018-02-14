09664721bec5715abc10db8443f1ca78041be30c
Zhengdong Wang <zhengdong.wang@gmail.com>
2018-02-14 16:46:07 -0500

    imported image preloader

--- alex.py
+++ alex.py
@@ -4,10 +4,11 @@ from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.normalization import local_response_normalization
 from tflearn.layers.estimator import regression
+from tflearn.data_utils import image_preloader
 
 # loading data
-dataPath = '/data'
-X, Y = image_preloader(path_data, image_shape=(120, 120),
+dataPath = '/dataExample'
+X, Y = image_preloader(dataPath, image_shape=(120, 120),
 						mode='folder', categorical_labels=True,
 						normalize=True)
 print('Dataset Loaded')
@@ -31,15 +32,15 @@ network = fully_connected(network, 4096, activation='tanh')
 network = dropout(network, 0.5)
 network = fully_connected(network, 17, activation='softmax')
 network = regression(network, optimizer='momentum',
-										 loss='categorical_crossentropy',
-										 learning_rate=0.001)
+						loss='categorical_crossentropy',
+						learning_rate=0.001)
 
 # training
 model = tflearn.DNN(network, checkpoint_path='alexCheckpoint',
-										max_checkpoints=1, tensorboard_verbose=2)
+						max_checkpoints=1, tensorboard_verbose=2)
 model.fit(X, Y, n_epoch=1000, validation_set=0.1, shuffle=True,
-					show_metric=True, batch_size=64, snapshot_step=200,
-					snapshot_epoch=False, run_id='alexSDSS')
+			show_metric=True, batch_size=64, snapshot_step=200,
+			snapshot_epoch=False, run_id='alexSDSS')
 
 
 
