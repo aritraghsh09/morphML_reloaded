85c3da4dcf43db1bd621bba64d1a5720937ad661
Aritra Ghosh <aritraghsh09@gmail.com>
2019-09-03 11:34:45 -0400

    New Updates

--- YaleHPCinfo/submit_job.sh
+++ YaleHPCinfo/submit_job.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-#SBATCH --job-name=gal_8e
-#SBATCH --output=gal_net_run_8e_n1.txt
+#SBATCH --job-name=galreg_00
+#SBATCH --output=galreg_test.txt
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:p100:1
 #SBATCH --partition=gpu
 #SBATCH --gres-flags=enforce-binding
-#SBATCH --time=18:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=aritra.ghosh@yale.edu
 
-export PATH=$HOME/anaconda2/bin:$PATH
-source activate tensorflow
-module load GPU/Cuda/8.0
-module load GPU/cuDNN/8.0-v6
+source /home/fas/urry/ag2422/.bashrc
+export PATH=$HOME/anaconda3/bin:$PATH
+conda init bash
+conda activate tf113
+module load cuDNN/7.6.0-CUDA-9.2.148.1
+module load CUDA/10.0.130
 
-python gal_net_mt.py
+python aritra_test.py
